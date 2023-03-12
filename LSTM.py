8894a2de7d31f313c10572e18f5f9efe9b4027b2
David Tyler Gosbee <65028334+Tylergos@users.noreply.github.com>
2023-03-12 15:23:44 -0300

    Adjusted LSTM, not finished

--- LSTM.py
+++ LSTM.py
@@ -4,6 +4,7 @@ import keras
 import keras.layers as layers
 import numpy as np
 import tensorflow as tf
+import torch
 
 
 def read_model(model_file: str = None):
@@ -15,22 +16,52 @@ def read_model(model_file: str = None):
     return weights
 
 
+# Generator for batching the data, as the data is too large to handle in one run
+# General idea from https://medium.com/analytics-vidhya/train-keras-model-with-large-dataset-batch-training-6b3099fdf366
+def batch_generator(x: np.ndarray, y: np.ndarray, batch_size: int, steps: int):
+    index = 0
+    while True:
+        yield (x[index*batch_size:index*batch_size + batch_size],
+               y[index*batch_size:index*batch_size + batch_size])
+        index = index + 1 if index < steps else 0
+
+
 class LSTM:
     def __init__(self, model_file: str = None) -> None:
         self.model = None
         read_model(model_file)
 
-    def train_model(self, train_x: tf.Tensor, train_y: tf.Tensor, test_x: tf.Tensor, test_y: tf.Tensor, output_file: str = None) -> None:
-        # classifications = np.unique(train_y.numpy())
-
+    def train_model(self,
+                    train_x: np.ndarray,
+                    train_y: np.ndarray,
+                    test_x: np.ndarray,
+                    test_y: np.ndarray,
+                    output_file: str = None,
+                    epochs: int = 3,
+                    steps_per_epoch: int = 5,
+                    batches: int = 1) -> None:
         model = keras.Sequential()
-        model.add(layers.Input(shape=(661504, 1,)))
+        model.add(layers.Input(shape=(train_x.shape[1], 1,)))
         model.add(layers.LSTM(100))
-        model.add(layers.Dense(50, activation="softmax"))
-        # model.add(layers.Dense(np.shape(classifications)[0], activation="softmax"))
-        model.compile(loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+        model.add(layers.Dense(train_y.shape[1], activation="softmax"))
+        model.compile(loss="categorical_crossentropy", metrics=["accuracy"])
 
-        model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=3, steps_per_epoch=5)
+        training_generator = batch_generator(train_x, train_y, batches, steps_per_epoch)
+
+        model.fit(train_x, train_y,
+                  batch_size=batches,
+                            validation_data=(test_x, test_y),
+                            epochs=epochs,
+                            steps_per_epoch=steps_per_epoch,
+                            verbose=1,
+                            use_multiprocessing=True)
+
+        # model.fit_generator(training_generator,
+        #                     validation_data=(test_x, test_y),
+        #                     epochs=epochs,
+        #                     steps_per_epoch=steps_per_epoch,
+        #                     verbose=1,
+        #                     use_multiprocessing=True)
 
         if output_file is not None:
             file = open(output_file, "wb")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import librosa
 import os
+
+import numpy
 import numpy as np
 import pickle
 import speech_recognition as sr
 import nltk
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 import tensorflow as tf
 
 from Lexicon import Lexicon
@@ -16,8 +18,9 @@ from keras.utils import pad_sequences
 DATASET_PATH = "./SpeakersDataset/50_speakers_audio_data"
 PICKLE_AUDIO_FILE_PATH = "./AudioData.txt"
 PICKLE_TEXT_FILE_PATH = "./TextData.txt"
-SAMPLE_RATE = 11025
-CLIP_LENGTH = 661504
+SAMPLE_RATE = 11025 // 4
+CLIP_LENGTH = 661504 // 4
+BATCH_SIZE = 64
 
 
 def read_and_save_audio(file_name: str = PICKLE_AUDIO_FILE_PATH) -> None:
@@ -64,9 +67,9 @@ def speech_to_text(file_name: str = PICKLE_TEXT_FILE_PATH, quick_run: bool = Fal
             except: # file is corrupted or has issues
                 print("File is corrupted or has issues")
         speaker_files[speaker] = text_files
-    pickle_file = open(file_name, "wb")
-    pickle.dump(speaker_files, pickle_file)
-    pickle_file.close()
+        pickle_file = open(file_name, "wb")
+        pickle.dump(speaker_files, pickle_file)
+        pickle_file.close()
 
 
 def read_saved_text(file_name: str = PICKLE_TEXT_FILE_PATH) -> dict[list[str]]:
@@ -76,19 +79,7 @@ def read_saved_text(file_name: str = PICKLE_TEXT_FILE_PATH) -> dict[list[str]]:
     return text_data
 
 
-def main():
-    # Running these will overwrite current data, ensure that you mean to do so before running it
-    # read_and_save_audio()
-    # speech_to_text(quick_run=True)
-
-    audio_data = read_saved_audio()
-    print(audio_data)
-    text_data = read_saved_text()
-
-    lexicon_model = Lexicon(text_data)
-
-    print(lexicon_model.classify(str(text_data["Speaker0033"])))
-
+def convert_audio_data(audio_data: dict[list[str]]) -> tuple[np.ndarray, np.ndarray]:
     x = []
     y = []
     for (author, data) in audio_data.items():
@@ -97,24 +88,56 @@ def main():
             x.append(clip[0])
             y.append(int(author[-2::]))
 
+    y = np.array(y)
+    y = tf.keras.utils.to_categorical(y, num_classes=len(numpy.unique(y)))
+
     # We need to pad the clips as they are not all exactly the same length
     x = pad_sequences(x, maxlen=CLIP_LENGTH, dtype=float)
-    y = np.array(y)
+    return x, y
+
+
+def train_and_run_LSTM_model(audio_data: dict[list[str]]) -> None:
+    x, y = convert_audio_data(audio_data)
+
+    train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=0.8)
+
+    lstm_model = LSTM()
+    lstm_model.train_model(train_x, train_y, test_x, test_y, steps_per_epoch=len(train_y) // BATCH_SIZE, batches=(len(test_y) // BATCH_SIZE) + 1)
+    lstm_model.evaluate(test_x, test_y)
+
+
+def train_and_run_LSTM_model_kfold(audio_data: dict[list[str]]) -> None:
+    x, y = convert_audio_data(audio_data)
 
     kfold = StratifiedKFold(5)
 
     accuracies = []
     for fold, (train_index, test_index) in enumerate(kfold.split(x, y)):
-        train_x = tf.convert_to_tensor(x[train_index])
-        train_y = tf.convert_to_tensor(y[train_index])
-        test_x = tf.convert_to_tensor(x[test_index])
-        test_y = tf.convert_to_tensor(y[test_index])
+        train_x = x[train_index]
+        train_y = y[train_index]
+        test_x = x[test_index]
+        test_y = y[test_index]
 
         lstm_model = LSTM()
-        lstm_model.train_model(train_x, train_y, test_x, test_y)
+        lstm_model.train_model(train_x, train_y, test_x, test_y, steps_per_epoch=len(train_y) // BATCH_SIZE, batches=(len(test_y) // BATCH_SIZE) + 1)
         accuracies.append(lstm_model.evaluate(test_x, test_y))
     print(accuracies)
 
+
+def main():
+    # Running these will overwrite current data, ensure that you mean to do so before running it
+    # read_and_save_audio()
+    # speech_to_text(quick_run=True)
+    speech_to_text()
+
+    # audio_data = read_saved_audio()
+    # print(audio_data)
+    # text_data = read_saved_text()
+
+    # lexicon_model = Lexicon(text_data)
+
+    # train_and_run_LSTM_model(audio_data)
+    # train_and_run_LSTM_model_kfold(audio_data)
 
 
 if __name__ == '__main__':
